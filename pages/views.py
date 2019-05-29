2894bab80fdd776b2cde2e598a92ea77a8bd1c8d
crycetruly <crycetruly@gmail.com>
2019-05-29 06:38:41 +0300

    ft(setup): Add pages,templates and base layout

--- pages/views.py
+++ pages/views.py
@@ -1,8 +1,10 @@
 from django.shortcuts import render
-from django.http import HttpResponse
 
-# Create your views here.
 
 
 def index(request):
-    return HttpResponse("<h1>Hello World</h1>")
+    return render(request, "pages/index.html")
+
+
+def about(request):
+    return render(request, "pages/about.html")

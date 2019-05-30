ab866b85d1470db71353909181df6d93be7cf685
crycetruly <crycetruly@gmail.com>
2019-05-30 23:17:32 +0300

    ft(media): Configure media files

--- realtors/models.py
+++ realtors/models.py
@@ -5,13 +5,12 @@ from django.db import models
 
 class Realtor(models.Model):
     name = models.CharField(max_length=255)
-    photo = models.ImageField(upload_to='')
+    photo = models.ImageField(upload_to="photos/%Y%m/%d/", blank=True)
     description = models.TextField()
     email = models.EmailField()
     phone = models.CharField(max_length=255)
     is_mvp = models.BooleanField(default=False)
     hire_date = models.DateTimeField(auto_now_add=True)
 
-
-def __str__(self):
-    return self.name
+    def __str__(self):
+        return self.name
